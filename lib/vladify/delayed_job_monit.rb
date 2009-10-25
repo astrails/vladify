b3505b6c549b46615017c1d53aec3bf9402ac69b
Vitaly Kushner <vitaly@astrails.com>
2009-10-25 22:31:19 +0200

    delayed_job with moni

---
+++ lib/vladify/delayed_job_monit.rb
@@ -0,0 +1,23 @@
+namespace :dj do
+  %w/start stop restart/.each do |c|
+    desc "#{c} delayed_job"
+    task c.to_sym do
+      system("sudo monit #{c} delayed_job-#{application}") || fail("Failed to #{c} delayed_job")
+    end
+  end
+end
+
+namespace :remote do
+  namespace :dj do
+    %w/start stop restart/.each do |c|
+      desc "#{c} delayed_job"
+      remote_task c, :roles => :app do
+        run "cd #{current_path} && RAILS_ENV=#{rails_env} sudo monit #{c}  delayed_job-#{application}"
+      end
+    end
+  end
+end
+
+namespace :deploy do
+  task :restart => "remote:dj:stop remote:dj:start"
+end
